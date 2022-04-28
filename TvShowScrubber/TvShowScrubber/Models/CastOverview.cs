3eeaef6cad791300fd6e917df396eee29b7bc982
Jaco van Zyl <jaconielvz@gmail.com>
2022-04-28 14:42:14 +0200

    Remove duplicates

--- TvShowScrubber/TvShowScrubber/Models/CastOverview.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace TvShowScrubber.Models
-{
-    public class CastOverview
-    {
-        [System.Text.Json.Serialization.JsonPropertyName("person")]
-        public Cast Person { get; set; }
-    }
-}
